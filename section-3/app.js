cc4bf5840926ee6faf1b5fb690991a4bc23efb90
Abdul Raffay <AbdulRaffay.Bashir@gmail.com>
2025-07-28 18:41:13 +0500

    something

--- section-3/app.js
+++ section-3/app.js
@@ -1,1 +1,9 @@
-"use strict";
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  req.on("close", () => {
+    console.log("closed!");
+  });
+});
+
+server.listen(3000);
